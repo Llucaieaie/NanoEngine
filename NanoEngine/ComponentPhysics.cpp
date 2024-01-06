a7d88267191f8673936e4086a3f786bba4d8d682
Lluc <99950000+Llucaieaie@users.noreply.github.com>
2024-01-06 13:32:34 +0100

    Physics component inspector

--- NanoEngine/ComponentPhysics.cpp
+++ NanoEngine/ComponentPhysics.cpp
@@ -28,5 +28,50 @@ void ComponentPhysics::Update()
 
 void ComponentPhysics::PrintInspector()
 {
+	if (ImGui::CollapsingHeader("Physics"))
+	{
+		ImGui::Checkbox("Static", &isStatic);
 
+		if (ImGui::Combo("Collider", reinterpret_cast<int*>(&shapeSelected), "Box\0Sphere\0Cylinder\0None"))
+
+		//	CheckShapes();
+
+		if (shapeSelected != ColliderShape::NONE) {
+			switch (shapeSelected)
+			{
+			case ComponentPhysics::ColliderShape::BOXSHAPE:
+			{
+				PrimCube cube;
+				collider = phys->AddBody(cube, 3.f);
+			}
+			break;
+			case ComponentPhysics::ColliderShape::SPHERESHAPE:
+			{
+				PrimSphere sphere;
+				collider = phys->AddBody(sphere, 3.f);
+			}
+			break;
+			case ComponentPhysics::ColliderShape::CYLINDERSHAPE:
+			{
+				PrimCylinder cylinder;
+				collider = phys->AddBody(cylinder, 3.f);
+			}
+			break;
+			default:
+				break;
+			}
+		}
+
+	//	if (ImGui::Button("Remove Collider"))
+	//	{
+	//		phys->RemoveBody(collider);
+	//		collider->~PhysBody3D();
+	//		collider = nullptr;
+	//	}
+	
+		if (ImGui::Button("Remove Component", ImVec2(140, 20)))
+		{
+
+		}
+	}
 }
